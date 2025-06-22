911597755143ae283d4e68930667ec636bfc0dcd
xliel <lielrahom321@gmail.com>
2025-06-22 03:12:18 +0300

    feat: add clan data to User class

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -3494,6 +3494,12 @@ export class Typing extends Base {
   };
 }
 
+export interface UserClan {
+  guildId: Snowflake;
+  badge: string;
+  tag: string;
+}
+
 export interface AvatarDecorationData {
   asset: string;
   skuId: Snowflake;
@@ -3517,6 +3523,7 @@ export class User extends Base {
   public accentColor: number | null | undefined;
   public avatar: string | null;
   public avatarDecorationData: AvatarDecorationData | null;
+  public clan: UserClan | null;
   public banner: string | null | undefined;
   public bot: boolean;
   public get createdAt(): Date;
